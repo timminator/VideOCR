7000a0fdcbc2f57005d3b5bcdfe09cc8f33e4b92
timminator <timalodemann@gmail.com>
2026-09-04 23:40:43 +0200

    Reenable Drag and Drop support for Linux
    Issue https://github.com/Eliav2/tkinterdnd2/issues/20 is fixed.

--- VideOCR.py
+++ VideOCR.py
@@ -2774,12 +2774,11 @@ else:
         window.refresh()
 
 # Register elements for Drag & Drop
-if sys.platform == "win32":
-    try:
-        dnd.register_element_dnd(window['-VIDEO-LIST-'], window, dnd.DROP_TYPE_FILES)
-        dnd.register_element_dnd(window['-GRAPH-'], window, dnd.DROP_TYPE_FILES)
-    except Exception as e:
-        log_error(f"Could not register Drag and Drop: {e}")
+try:
+    dnd.register_element_dnd(window['-VIDEO-LIST-'], window, dnd.DROP_TYPE_FILES)
+    dnd.register_element_dnd(window['-GRAPH-'], window, dnd.DROP_TYPE_FILES)
+except Exception as e:
+    log_error(f"Could not register Drag and Drop: {e}")
 
 # --- Load settings when the application starts ---
 load_settings(window)
